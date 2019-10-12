8e781086c886e9107b93c0463ae1941f7da13445
WarlockMorgana <47517278+WarlockMorgana@users.noreply.github.com>
2019-10-12 00:03:03 -0400

    Update Rules.java

--- Rules.java
+++ Rules.java
@@ -7,19 +7,27 @@ public class Rules {
 	//1 = high card, 2 = one pair, 3 = two pair, 4 = three of a kind, 5 = straight, 6 = flush, 7 = full house, 8 = four of a kind, 9 = straight flush
 	
 	public Rules() {
-		int[] rules = {9,8,7,6,5,4,3,2,1};
+		int[] r = {9,8,7,6,5,4,3,2,1};
+		rules = r;
 	}
 	
 	public Rules(int min) {
 		if(min==2) {
-			int[] rules = {9,8,7,6,5,4,3,2,1};
+			int[] r = {9,8,7,6,5,4,3,2,1};
+			rules = r;
 		}
 		else if(min<9) {
-			int[] rules = {9,8,6,7,5,4,3,2,1};
+			int[] r = {9,8,6,7,5,4,3,2,1};
+			rules = r;
 		}
 		else {
-			int[] rules = {9,6,8,5,7,4,3,2,1};
+			int[] r = {9,6,8,5,7,4,3,2,1};
+			rules = r;
 		}
+	}
+	
+	public int[] getRules() {
+		return rules;
 	}
 
 }
